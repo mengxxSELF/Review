81adedeede2b18f497a624ccf40969cbf0d77271
webmaxxSELF <webmxxself@163.com>
2017-01-13 18:06:27 +0800

    other

--- other/bind.js
+++ other/bind.js
@@ -1,17 +1,19 @@
-/*bind */
+/*bind  ->柯里化函数 */
 
 // call apply bind 都属于Function原型的方法
 // bind 更改this指向 返回一个函数定义阶段  fn2.bind(newThis,arg1,...)
 
 Function.prototype.myBind= function (context) {
     var args = Array.prototype.slice.call(arguments,1); // 获取参数 -》数组格式
-    console.log(args)
     if('bind' in Function.prototype){
-        return this.bind.apply(context,[context].concat(args));
+        return this.bind.apply(this,[context].concat(args));
     }
     var _this =this;
     // 在不支持bind方法时
     return function () {
+        var e=window.event;// ie 放入事件对象
+        args.push(e);
         _this.apply(context,args);
     };
 };
+
