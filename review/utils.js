5b54a81682f84efc6e433b34c4e8fdef20302622
webmaxxSELF <webmxxself@163.com>
2017-01-15 14:03:28 +0800

    utils

--- review/utils.js
+++ review/utils.js
@@ -30,6 +30,52 @@ var utils = function () {
         }
         return domAry;
     }
+
+    function getCss(obj,attr){
+        // 获取元素某一个样式 opacity
+        var val=null , reg=null;
+        if(flag){
+            return getComputedStyle(obj,false)[attr];
+        }else{
+            // ie
+            // opacity
+            if(attr=='opacity'){
+                val = obj.currentStyle['filter']; // 60    filter:alpha(opacity=60)
+                reg=/^alpha\(opacity[:=](\d+)\)$/i;
+                val =  reg.test(val)? RegExp.$1/100:1;
+            }else{
+                val = obj.currentStyle[attr];
+            }
+        }
+        //处理单位 12 px  rem pt
+        reg= /^([-+])?(\d+(\.\d+)?)(px|pt|rem|em)?$/i;
+
+        return reg.test(val)?parseFloat(val):val;
+    }
+
+    function setCss(obj,attr,val){
+        // 设置一个样式
+        // 处理 float opacity 单位
+
+        if(attr=='float'){
+            obj.style.cssFloat=val;
+            obj.style.styleFloat=val;
+        }
+         if(attr=='opacity'){
+            obj.style.opacity=val;
+            obj.style.filter= 'alpha(opacity='+ val*100 +')' ; // 注意乘以100
+        }
+        // 处理单位 width height left top right bottom  margin|padding|
+        var reg= /^(width|height|left|top|right|bottom|((margin|padding)(left|top|right|bottom)?))$/i;
+        // 如果是 %  auto
+        if(reg.test(attr)){
+            if(!(/(%|auto)/i).test(val)){
+                //val+='px'; // 加上单位
+                val= parseInt(val)+'px'; // 原来可以 有单位 也可以没有
+            }
+       }
+        obj.style[attr]=val;
+    }
     return{
         //getByClass 根据class获取元素
         getByClass:getByClass,
@@ -40,9 +86,9 @@ var utils = function () {
         //10 removeClass 给元素移除class
 
             //11 getCss 获取css 样式
-
+        getCss:getCss,
             //12 setCss 设置一个css样式
-
+        setCss:setCss,
         //13 setGroupCss 批量设置css
 
             //14 css 集 获取 设置一体
